fbe620912417f82b22f964142c6faa5280cce05c
ODBabalola <u18041494@tuks.co.za>
2021-06-01 23:37:47 +0200

    Made styling changes to CSS

--- src/app/import/import.component.ts
+++ src/app/import/import.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ImportComponent implements OnInit   {
 
   clicked = false;
-  title = "import";
+  title = "Import";
 
   constructor() { }
 
