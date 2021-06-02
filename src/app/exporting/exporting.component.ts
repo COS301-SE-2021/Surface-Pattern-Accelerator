269c1d22b206d9358948a3c89f2eccca06fbe0da
olivia9469 <82371348+olivia9469@users.noreply.github.com>
2021-06-02 11:23:04 +0200

    Components created

---
+++ src/app/exporting/exporting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exporting',
+  templateUrl: './exporting.component.html',
+  styleUrls: ['./exporting.component.css']
+})
+export class ExportingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
