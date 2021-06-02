77b137308c045410e988dfb6948498660f792a6f
Reghardt <reghardt1000@gmail.com>
2021-06-02 09:57:24 +0200

    Created a collections component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CollectionsComponent } from './collections/collections.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CollectionsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/collections/collections.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-collections',
+  templateUrl: './collections.component.html',
+  styleUrls: ['./collections.component.css']
+})
+export class CollectionsComponent implements OnInit {
+
+  constructor() { }
+
+  collection = "some collection";
+
+  ngOnInit(): void {
+  }
+
+}
