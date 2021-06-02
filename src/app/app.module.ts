ba5bd65e1c11009513c46189f565d31ae64b928f
JohanDC-CS <u18200754@tuks.co.za>
2021-06-02 10:39:11 +0200

    ExportingComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExportingComponent } from './exporting/exporting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExportingComponent
   ],
   imports: [
     BrowserModule,
