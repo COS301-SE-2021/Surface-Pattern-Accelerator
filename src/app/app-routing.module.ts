78cadf38090f0eef0dff176b9183651bfe03f8a8
ODBabalola <u18041494@tuks.co.za>
2021-06-03 05:23:54 +0200

    Added Navbar to AppComponent. Tested switching views.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ImportComponent} from "./import/import.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'import', component: ImportComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
