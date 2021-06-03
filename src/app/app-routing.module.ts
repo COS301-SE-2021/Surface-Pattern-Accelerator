52af7d48e827c57bea9789eccba39c9e645ab6b3
ODBabalola <u18041494@tuks.co.za>
2021-06-03 06:34:30 +0200

    Pattern Component code, Navbar adjustments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ImportComponent} from "./import/import.component";
+import { PatternComponent } from "./pattern/pattern.component";
 
 
 const routes: Routes = [
-  { path: 'import', component: ImportComponent }
+  { path: 'import', component: ImportComponent },
+  { path: 'pattern', component: PatternComponent}
 ];
 
 @NgModule({
