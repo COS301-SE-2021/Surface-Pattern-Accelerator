b08fc2f12e80e5e0b55a87d4a00b5677bfef577d
Bernadette404 <56558453+Bernadette404@users.noreply.github.com>
2021-06-03 15:00:00 +0200

    Update collection-creator.component.ts
    Updated collection component

--- src/app/collection-creator/collection-creator.component.ts
+++ src/app/collection-creator/collection-creator.component.ts
@@ -9,17 +9,19 @@ import { CollectionsService } from '../collections.service'
 })
 export class CollectionCreatorComponent implements OnInit {
 
-  collections: Collection[] = []; //the collections that get displayed
+  //the collections that get displayed
+  collections: Collection[] = []; 
 
   constructor(private collectionsService: CollectionsService) { }
 
-  getCollections(): void //this funct gets called each time this component gets initialized
+  //this funct gets called each time this component gets initialized
+  getCollections(): void 
   {
     this.collectionsService.getCollections()                      //this is an asyncronous operation because it gets data from a server, a server does not return instantly
       .subscribe(collections => this.collections = collections);  //assigns data from server to local collections variable, gets an Observable
   }
 
-  //add new collection
+  //add a new collection
   add(name: string): void {
     name = name.trim();
     if (!name) { return; }
