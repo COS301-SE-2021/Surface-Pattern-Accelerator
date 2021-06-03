d41f72cd3c92d5d68be852b4a1a9f6496c95e359
ODBabalola <u18041494@tuks.co.za>
2021-06-03 13:59:28 +0200

    Testing out simple unit tests

--- src/app/import/import.component.spec.ts
+++ src/app/import/import.component.spec.ts
@@ -19,7 +19,14 @@ describe('ImportComponent', () => {
     fixture.detectChanges();
   });
 
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('checking if upload label is correct', () => {
+    const data  = fixture.nativeElement;
+    expect(data.querySelector(".custom-file-label").textContent).toContain("Choose Image");
+  })
 });
