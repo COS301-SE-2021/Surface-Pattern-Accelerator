72612a81570e69697b5dd0b65640e7f2477573f3
Bernadette404 <56558453+Bernadette404@users.noreply.github.com>
2021-06-03 14:47:08 +0200

    Update pattern.component.ts
    Pattern component updated

--- src/app/pattern/pattern.component.ts
+++ src/app/pattern/pattern.component.ts
@@ -9,7 +9,7 @@ export class PatternComponent implements OnInit {
 
   title = 'testAngular';
   username: string | undefined;
-  iterations: number = 18;
+  spacing: number = 18;
   rotate : string | undefined;
   rotateNum: number = 0;
 
@@ -17,16 +17,16 @@ export class PatternComponent implements OnInit {
     document.addEventListener("change", ()=>{
       console.log("yes");
       this.username = (<HTMLInputElement>document.getElementById("name")).value;
-      this.iterations = +this.username;
+      this.spacing = +this.username;
 
       this.rotate = (<HTMLInputElement>document.getElementById("rotate")).value;
       this.rotateNum = +this.rotate;
 
-      console.log(this.iterations);
-      this.setSize(this.iterations, this.rotateNum);
+      console.log(this.spacing);
+      this.setSize(this.spacing, this.rotateNum);
 
     })
-    this.setSize(this.iterations, this.rotateNum);
+    this.setSize(this.spacing, this.rotateNum);
     let downloadBtn = <HTMLElement> document.getElementById("download-btn");
     if(downloadBtn){
       downloadBtn.addEventListener("click", () => {
@@ -36,44 +36,31 @@ export class PatternComponent implements OnInit {
         let newFilename;
         newFilename =  "canvas-edited.jpg";
 
-
         // Call download
         canvas = <HTMLCanvasElement> document.getElementById("myCanvas");
         //this.download(canvas, newFilename);
         this.downloadCanvas(canvas);
 
-
-
       });
     }
-
-
-
 
   }
 
 
-
-  setSize(iterations: number, rotateNum: number) {
+  setSize(spacing: number, rotateNum: number) {
     console.log("yes");
     let c = <HTMLCanvasElement> document.getElementById("myCanvas");
     c.width = (window.innerWidth);
 
     const ctx =<CanvasRenderingContext2D> c.getContext("2d");
 
-    var campfire = new Image();
-    campfire.style.width = "1";
-    console.log(campfire.width);
+    var motif = new Image();
+    motif.style.width = "1";
+    console.log(motif.width);
     ctx.rotate(rotateNum);
 
-    campfire.onload = function () {
+    motif.onload = function () {
       let k = 1;
-      // for(let i = 0 ; i < 8; i++){
-      // ///////work /////////////
-      // ctx.drawImage(campfire, k*5, /*-340*/10, /*150*/180, /*c.height*2*/150);
-
-      // k+=20;
-      // }
 
       k=1;
       let l = 1;
@@ -84,24 +71,19 @@ export class PatternComponent implements OnInit {
         ///////work /////////////yes
         for(let j = 0 ; j < 100 ; j++){
           if(l*5<window.innerWidth || k*8<window.innerHeight)
-            ctx.drawImage(campfire, l*5, /*-340*/8*k, /*150*/180, /*c.height*2*/150);
+            ctx.drawImage(motif, l*5, /*-340*/8*k, /*150*/180, /*c.height*2*/150);
 
-          k+=iterations;
+          k+=spacing;
         } l+=20;
       }
 
     };
-    campfire.src = "../assets/thread.svg";
-
+    motif.src = "../assets/thread.svg";
 
   }
 
-
-
 // Download
   download(canvas : HTMLCanvasElement, filename : string) {
-
-
     // Init event
     let e;
     // Create link
