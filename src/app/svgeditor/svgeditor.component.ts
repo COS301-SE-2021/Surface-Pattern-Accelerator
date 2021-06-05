d1b47457fcf5748d8dfd630c895fa73eb7386b94
ODBabalola <u18041494@tuks.co.za>
2021-06-05 14:15:54 +0200

    Further functionality being added to the SVGeditor.

--- src/app/svgeditor/svgeditor.component.ts
+++ src/app/svgeditor/svgeditor.component.ts
@@ -3,8 +3,12 @@ import { Component, OnInit } from '@angular/core';
   Resources used to understand and install Angular material:
   https://material.angular.io/guide/getting-started
   https://material.angular.io/components/button/api
+
+  For future reference:
+  https://angular.io/guide/svg-in-templates
  */
-import {MatButtonModule} from '@angular/material/button';
+//import {MatButtonModule} from '@angular/material/button';
+//import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-svgeditor',
@@ -16,16 +20,26 @@ export class SVGeditorComponent implements OnInit {
       Resources used to understand this implementation:
       https://blog.angular-university.io/angular-file-upload/
    */
-  fileName: string;
+
+  fileName = "";
+  requiredFileType: any;
 
   constructor() {
-    this.fileName = "";
   }
 
   ngOnInit(): void {
   }
 
-  onFileSelected($event: Event) {
 
+  onFileSelected($event: Event) {
+    //The ! operator shows typescript my confidence that a null value will not be returned
+    //The event is also type-casted as a HTMLInputElement, so the compiler can assess the files element
+    // @ts-ignore
+    const file:File = ($event.target as HTMLInputElement).files[0];
+    if (file) {
+      this.fileName = file.name;
+    }
   }
+
+
 }
