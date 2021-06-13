cf0b04fe5057218885c9a831e0cd1131bcf72a72
EmilioCoelho-ui <u18194380@tuks.co.za>
2021-06-13 18:17:48 +0200

    added routing for workarea

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { CollectionsComponent } from './components/collections/collections.compo
 import { CollectionCreatorComponent } from './components/collection-creator/collection-creator.component';
 import { PatternComponent } from './components/pattern/pattern.component';
 import { ImportComponent} from './components/import/import.component';
+import { WorkareaComponent} from './components/workarea/workarea.component';
 
 const routes: Routes = [
   { path: 'home',    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)  },
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: 'collectionCreator', component: CollectionCreatorComponent},
   { path: 'pattern', component: PatternComponent},
   { path: 'import', component: ImportComponent },
+  { path: 'workarea', component: WorkareaComponent },
 ];
 
 @NgModule({
