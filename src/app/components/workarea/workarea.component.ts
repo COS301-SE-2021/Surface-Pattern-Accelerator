18adb32290b8affd0a06c9b1e766a9d88bdb37ec
EmilioCoelho-ui <u18194380@tuks.co.za>
2021-06-13 18:18:57 +0200

    added new ionic component for workarea

---
+++ src/app/components/workarea/workarea.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-workarea',
+  templateUrl: './workarea.component.html',
+  styleUrls: ['./workarea.component.scss'],
+})
+export class WorkareaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
