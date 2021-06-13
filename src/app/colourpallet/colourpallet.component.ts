ecb83eb53dade56bd420f77de103edc0a28fe353
olivia9469 <82371348+olivia9469@users.noreply.github.com>
2021-06-13 19:52:10 +0200

    colour pallet file3

---
+++ src/app/colourpallet/colourpallet.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-colourpallet',
+  templateUrl: './colourpallet.component.html',
+  styleUrls: ['./colourpallet.component.scss'],
+})
+export class ColourpalletComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
