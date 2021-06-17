f976541e8535ffbd976bfc2b1148a563163b8291
olivia9469 <82371348+olivia9469@users.noreply.github.com>
2021-06-17 19:14:30 +0200

    creating colorGen function

---
+++ src/app/components/collor-pallet/collor-pallet.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-collor-pallet',
+  templateUrl: './collor-pallet.component.html',
+  styleUrls: ['./collor-pallet.component.scss'],
+})
+export class CollorPalletComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  colorGen(){}
+
+}
