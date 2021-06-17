f9fb133f0f159fa955754a80654dec2651798d3b
ODBabalola <u18041494@tuks.co.za>
2021-06-17 15:43:06 +0200

    CSS change in patter navbar.

--- src/app/sidenav/sidenav.page.ts
+++ src/app/sidenav/sidenav.page.ts
@@ -6,28 +6,9 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./sidenav.page.scss'],
 })
 export class SidenavPage implements OnInit {
-  //Variable declarations
-  dropdown;
-  i : number;
+
 
   ngOnInit() {
-    document.addEventListener("change", ()=>{
-      console.log("Sidenav started.");
-      this.dropdown = (<HTMLElement><unknown>document.getElementsByClassName("dropdown-btn"));
-
-      for (this.i = 0; this.i < this.dropdown.length; this.i++) {
-        this.dropdown[this.i].addEventListener('click', function () {
-          this.classList.toggle("active");
-          let dropdownContent = this.nextElementSibling;
-          if (dropdownContent.style.display === "block") {
-            dropdownContent.style.display = "none";
-          } else {
-            dropdownContent.style.display = "block";
-          }
-        });
-      }
-    })
-
   }
 
 }
