bf84e8f022d20aa8c49add6fc6ca58f58f4242e7
ODBabalola <u18041494@tuks.co.za>
2021-06-17 15:16:51 +0200

    Linking SideNav Component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,22 @@ import { PatternComponent } from './components/pattern/pattern.component';
 import { ImportComponent} from './components/import/import.component';
 
 const routes: Routes = [
+
   { path: 'home',    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)  },
-  { path: '',    redirectTo: 'collections',    pathMatch: 'full'  },
+  // I changed the path from '' to 'collect' just to develop the sidenav globally
+  // Can be reached from the navigation bar
+  { path: 'collect',    redirectTo: 'collections',    pathMatch: 'full'  },
   { path: 'collections', component: CollectionsComponent}, //if a url matches this path then the appropriate component wil be displayed
   { path: 'collectionCreator', component: CollectionCreatorComponent},
   { path: 'pattern', component: PatternComponent},
   { path: 'import', component: ImportComponent },
+
+
+  {
+    path: '',
+    loadChildren: () => import('./sidenav/sidenav.module').then( m => m.SidenavPageModule)
+  },
+
 ];
 
 @NgModule({
