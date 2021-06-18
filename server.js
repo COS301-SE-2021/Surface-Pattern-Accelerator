b355c3efbbae3c731639aba7e836de8d52826c40
Reghardt <reghardt1000@gmail.com>
2021-06-18 17:07:48 +0200

    Server generates access code and stores in session correctly

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const SCOPES = ['https://www.googleapis.com/auth/drive'];
 // time.
 const TOKEN_PATH = 'token.json'; //this file is the users access token for when they gave acccess
 
+let auth;
 
 app.use(cors({origin: //cors so it can work with application on another domain
   ["http://localhost:8100"],
@@ -149,15 +150,22 @@ app.get('/api/newCollection/:tagId', function(req, res) {
   //const strWindowFeatures = 'toolbar=no, menubar=no, width=600, height=700, top=100, left=100';
   //window.open(authorize(JSON.parse(content)), 'sign in', strWindowFeatures);
   // Load client secrets from a local file.
-  fs.readFile('credentials.json', (err, content) => {
+  fs.readFile('credentials.json', (err, content) => 
+  {
     if (err) return console.log('Error loading client secret file:', err);
     // Authorize a client with credentials, then call the Google Drive API.
     console.log("get url");
-    res.json({ 'signInURL': authorize(JSON.parse(content))}); //send JSON with sign in URL
+    credentials = JSON.parse(content);
+    const {client_secret, client_id, redirect_uris} = credentials.installed; //disassemble JSON file
+    const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+    authUrl = oAuth2Client.generateAuthUrl({access_type: 'offline', scope: SCOPES});
+    auth = oAuth2Client;
+    res.json({ 'signInURL': authUrl}); //send JSON with sign in URL
   });
 
   //res.send("test");
 });
+
 
 
 
@@ -169,62 +177,29 @@ app.get('/api/newCollection/:tagId', function(req, res) {
  */
 //authorize generates a URL used to get access to a users Drive
 function authorize(credentials) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed; //'installed' name in  json file
-  const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
+  // const {client_secret, client_id, redirect_uris} = credentials.installed; //disassemble JSON file
+  // const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
 
-    return  authUrl = oAuth2Client.generateAuthUrl({
-      access_type: 'offline',
-      scope: SCOPES,
-    });
+    // return  authUrl = oAuth2Client.generateAuthUrl({
+    //   access_type: 'offline',
+    //   scope: SCOPES,
+    // });
 
-  // Check if we have previously stored a token.
-  // fs.readFile(TOKEN_PATH, (err, token) => {
-  //   // if (err) return getAccessToken(oAuth2Client, callback);
-  //   // oAuth2Client.setCredentials(JSON.parse(token));
-  //   // callback(oAuth2Client);
-  //   const authUrl = oAuth2Client.generateAuthUrl({
-  //     access_type: 'offline',
-  //     scope: SCOPES,
-  //   });
-  //   //console.log(authUrl);
-  //   //return authUrl;
-  // });
-  
-}
+    //did we previsoulsy store a token?
+    fs.readFile(TOKEN_PATH, (err, token) => { //token is the read buffer
+    if (err)
+    {
+      return getAccessToken(oAuth2Client);
+    }
 
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
-function getAccessToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
+    oAuth2Client.setCredentials(JSON.parse(token)); //convert buffer that is a json to an object
+    auth = oAuth2Client;
   });
-
-  
-  // console.log('Authorize this app by visiting this url:', authUrl);
-  // const rl = readline.createInterface({
-  //   input: process.stdin,
-  //   output: process.stdout,
-  // });
-  // rl.question('Enter the code from that page here: ', (code) => {
-  //   rl.close();
-  //   oAuth2Client.getToken(code, (err, token) => {
-  //     if (err) return console.error('Error retrieving access token', err);
-  //     oAuth2Client.setCredentials(token);
-  //     // Store the token to disk for later program executions
-  //     fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-  //       if (err) return console.error(err);
-  //       console.log('Token stored to', TOKEN_PATH);
-  //     });
-  //     callback(oAuth2Client);
-  //   });
-  // });
 }
+
+
+
+
 
 /**
  * Lists the names and IDs of up to 10 files.
@@ -251,30 +226,45 @@ function listFiles(auth) {
 
 
 
-app.post('/api/setAccessToken', (req, res) => { //read about express middlewares, like validatePayload
+app.post('/api/consumeAccessCode', (req, res) => { //read about express middlewares, like validatePayload
   
-  if(req.session.accessToken)
-  {
-    req.session.accessToken = req.body.accessToken; //reset access token for now while in development
-    console.log("access token is already set " + req.session.accessToken);
-    res.status(200).send({
-      Message: 'Access token is already set!'
-    });
+  // if(req.session.accessCode)
+  // {
+  //   req.session.accessToken = req.body.accessToken; //reset access token for now while in development
+  //   console.log("access token is already set " + req.session.accessToken);
+  //   res.status(200).send({
+  //     Message: 'Access token is already set!'
+  //   });
 
-  }
-  else
-  {
-    req.session.accessToken = req.body.accessToken;
-    console.log("Access token is now set: " + req.body.accessToken);
-    res.status(200).send({
-      Message: 'Access token is now set!'
+  // }
+  // else
+  // {
+  //   req.session.accessToken = req.body.accessToken;
+  //   console.log("Access token is now set: " + req.body.accessToken);
+  //   res.status(200).send({
+  //     Message: 'Access token is now set!'
+  // });
+
+  // }
+
+  auth.getToken(req.body.accessCode, (err, token) => {
+    if(err)
+    {
+      return console.log('Error retreiving token', err);
+    }
+
+    //store token to disk
+    fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+      if(err)
+      {
+        return console.error(err);
+      }
+      console.log('Token stored to', TOKEN_PATH)
+    });
+    //auth = oAuth2Client;
   });
 
-  }
 
-  // res.status(403).send({
-  //     errorMessage: 'Permission denied!'
-  // });
   
 });
 
