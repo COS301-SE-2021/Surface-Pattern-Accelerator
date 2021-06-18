8133f0b15365b2049249ec499a0c0a9acafb8b11
olivia9469 <82371348+olivia9469@users.noreply.github.com>
2021-06-18 20:35:53 +0200

    adding variables for unit testing

--- src/app/components/collor-pallet/collor-pallet.component.ts
+++ src/app/components/collor-pallet/collor-pallet.component.ts
@@ -14,12 +14,10 @@ export class CollorPalletComponent implements OnInit {
   colorGen(){
 
     const code = document.querySelectorAll('.code');
-
     const color = document.querySelectorAll('.color');
-
     const letters = '0123456789abcdef';
-
     const hashtag = ['#','#','#','#','#','#'];
+    var test: boolean;
 
     for (let i=0;i<6;i++){
       hashtag[i]+=letters[Math.floor(Math.random()*16)];
@@ -37,13 +35,18 @@ export class CollorPalletComponent implements OnInit {
       // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
       const c = <HTMLElement> color[i];
       c.style.backgroundColor = hashtag[i];
+      test = true;
     }
+    return test;
   }
 
   lockColour(id: string){
     const elem = document.getElementById(id);
     let prev: HTMLElement;
     let next: HTMLElement;
+    var testLock: boolean;
+    var testChange: boolean;
+
     if(elem) {
       // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
       prev = <HTMLElement>elem.previousElementSibling;
@@ -51,6 +54,7 @@ export class CollorPalletComponent implements OnInit {
       // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
       next = <HTMLElement>elem.nextElementSibling;
       console.log('p class: ' + next.className);
+      testChange = true;
     }
 
     if(prev.className === 'color'){
@@ -61,16 +65,20 @@ export class CollorPalletComponent implements OnInit {
       next.classList.remove('code');
       next.classList.add('new-code');
       elem.innerHTML = 'Unlock';
+      testLock = true;
     }
-    else{
+    else if(prev.className === 'stay-color'){
       prev.classList.remove('stay-color');
       prev.classList.add('color');
       prev.style.backgroundColor = next.innerHTML;
       next.classList.remove('new-code');
       next.classList.add('code');
       elem.innerHTML = 'Lock';
+      testLock = true;
     }
+
+    return testChange === testLock;
   }
 
-  
+
 }
