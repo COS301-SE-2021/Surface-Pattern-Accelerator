1d55eb966f162a6554008b10dd620dad34bb5dc8
EmilioCoelho-ui <u18194380@tuks.co.za>
2021-06-19 14:32:53 +0200

    added typescript functionality of Canvas size change

--- src/app/components/workarea/workarea.component.ts
+++ src/app/components/workarea/workarea.component.ts
@@ -8,11 +8,37 @@ import { Component, OnInit } from '@angular/core';
 export class WorkareaComponent implements OnInit {
 
   contentScrollActive = true;
+  width = 500;
+  height = 500;
+  rwidth = 800;
+  rect = 0;
+  check = false;
   constructor() {
   }
 
   ngOnInit()
   {}
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  chooseCanvasSymbol(check){
+    const myFrame = document.getElementById('canvasFrame');//get div of canvas frame
+    if(check===true)//square is alright
+    {
+      check = true;
+      this.width = 500;
+      this.height = 500;
+      myFrame.style.width = this.width + 40+ 'px';
+      myFrame.style.height = this.height + 40 + 'px';
+    }
+    else{//rectangle is option
+      check=false;
+      this.width = 800;
+      this.height = 500;
+      myFrame.style.width = this.rwidth + 40 + 'px';
+      myFrame.style.height = this.height + 40 +  'px';
+    }
+  }
+
+
 
   saveCanvasImage()
   {
