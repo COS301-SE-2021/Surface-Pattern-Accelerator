f0b172b6170b963ce2f785f712239391418cf3b4
ODBabalola <u18041494@tuks.co.za>
2021-06-20 19:47:40 +0200

    Added test for preview Popover component.

--- src/app/components/export-popover/export-popover.component.spec.ts
+++ src/app/components/export-popover/export-popover.component.spec.ts
@@ -1,4 +1,4 @@
-import {  async, ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import {  ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { ExportPopoverComponent } from './export-popover.component';
