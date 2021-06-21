3f8f70452118ddf1bcb10cafa488b808bbfa5b71
olivia9469 <82371348+olivia9469@users.noreply.github.com>
2021-06-21 07:59:32 +0200

    integration

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,20 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { TestBed, waitForAsync } from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { PatternComponent } from "./components/pattern/pattern.component";
+import { PreviewComponent } from "./components/preview/preview.component";
 
 describe('AppComponent', () => {
+
+  let patternComponent: PatternComponent;
+  let patternFixture: ComponentFixture<PatternComponent>;
+  let previewFixture: ComponentFixture<PreviewComponent>;
 
   beforeEach(waitForAsync(() => {
 
     TestBed.configureTestingModule({
-      declarations: [AppComponent],
+      declarations: [AppComponent, PatternComponent, PreviewComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   }));
@@ -19,5 +25,44 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
   // TODO: add more tests!
+
+  it('INTEGRATION: Call to pattern preview component works from the pattern component', () => {
+    if(patternFixture){
+
+      let elem = patternFixture.debugElement.nativeElement.querySelector('#preview');
+      elem.click();
+
+      expect(patternComponent.openModal).toHaveBeenCalled();
+    }
+  });
+
+  it('INTEGRATION: should ensure the path the image in function', function () {
+    if(patternFixture){
+
+      let elem = patternFixture.debugElement.nativeElement.querySelector('#preview');
+      elem.click();
+
+      expect(patternComponent.openModal).toBe(true);
+    }
+  });
+
+  it('INTEGRATION: should ensure the path the image uses is correct in src', function () {
+    if(patternFixture){
+
+      let patternElem = patternFixture.debugElement.nativeElement.querySelector('#preview');
+      patternElem.click();
+    }
+
+    if(previewFixture){
+
+      let elem = patternFixture.debugElement.nativeElement.querySelector('img');
+      elem.click();
+
+      expect(elem.src).toBe('../../assets/pattern-preview.png');
+    }
+  });
+
+
+
 
 });
