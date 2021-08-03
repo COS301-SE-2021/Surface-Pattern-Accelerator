7ab7161baa832e53fe45711d6fe592a46660fec5
JohanDC-CS <u18200754@tuks.co.za>
2021-08-03 11:50:18 +0200

    Draw grid

--- src/app/components/preview/preview.component.ts
+++ src/app/components/preview/preview.component.ts
@@ -11,4 +11,33 @@ export class PreviewComponent implements OnInit {
 
   ngOnInit() {}
 
+/* In Konva make this a layer */
+  addGrid(e) {
+    const canvas = <HTMLCanvasElement> document.getElementById('myCanvas');
+    const context = canvas.getContext('2d');
+    if(e.checked){
+      for (var x = 0.5; x < canvas.width; x += 10) {
+        context.moveTo(x, 0);
+        context.lineTo(x, 381);
+      }
+
+      for (var y = 0.5; y < canvas.height; y += 10) {
+        context.moveTo(0, y);
+        context.lineTo(500, y);
+      }
+
+
+
+      context.strokeStyle = "rgba(0,0,0,0.1)";
+      context.stroke();
+
+    }
+    else
+    {
+      context.clearRect(0,0,canvas.width,canvas.height);
+    }
+
+  }
+
+
 }
