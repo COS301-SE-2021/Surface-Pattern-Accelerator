4619f3fd240838411a38bbe1d119e5f2b4aec576
Reghardt <reghardt1000@gmail.com>
2021-08-03 14:24:50 +0200

    Konva implementation in pattern component

--- src/app/components/pattern/pattern.component.ts
+++ src/app/components/pattern/pattern.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController, PopoverController } from '@ionic/angular';
-import { ExportPopoverComponent } from '../export-popover/export-popover.component';
-import { PreviewComponent } from '../preview/preview.component';
+import Konva from "konva";
 
 @Component({
   selector: 'app-pattern',
@@ -9,21 +7,35 @@ import { PreviewComponent } from '../preview/preview.component';
   styleUrls: ['./pattern.component.scss'],
 })
 export class PatternComponent implements OnInit {
-
-  showFiller = false;
-
-  title = 'testAngular';
-  username: string | undefined;
-  spacing: number = 18;
-  rotate : string | undefined;
-  rotateNum: number = 0;
-  scale : string | undefined;
-  scaleNum: number = 0.7;
+  stage!: Konva.Stage;
+  layer!: Konva.Layer;
 
   ngOnInit(){
-    document.addEventListener("change", ()=>{
+    let width = window.innerWidth * 0.9;
+    let height = window.innerHeight;
+    this.stage = new Konva.Stage({
+      container: 'container',
+      width: width,
+      height: height
+    });
+    this.layer = new Konva.Layer();
+    this.stage.add(this.layer);
+    //this.addLineListeners();
 
-    })
+    const path = new Konva.Path({
+      x: 0,
+      y: 0,
+      data:
+        'M0 0h24v24H0V0z',
+      fill: 'green',
+      scale: {
+        x: 10,
+        y: 10,
+      },
+    });
+
+    // add the shape to the layer
+    this.layer.add(path);
   }
 
 
