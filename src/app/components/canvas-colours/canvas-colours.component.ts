b6bf0d87ad25e9a265efd9dd234b0e4de6dd7104
olivia9469 <82371348+olivia9469@users.noreply.github.com>
2021-08-03 10:20:53 +0200

    component.ts file for the canvas

---
+++ src/app/components/canvas-colours/canvas-colours.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-canvas-colours',
+  templateUrl: './canvas-colours.component.html',
+  styleUrls: ['./canvas-colours.component.scss'],
+})
+export class CanvasColoursComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {}
+
+}
