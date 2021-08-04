950c06508202066bccdc9c553b942f0df1be5f3e
ODBabalola <u18041494@tuks.co.za>
2021-08-04 16:22:39 +0200

    Changed Tab Icon Layout

--- src/app/components/color/color.component.ts
+++ src/app/components/color/color.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./color.component.scss'],
 })
 export class ColorComponent implements OnInit {
+  svgName: any;
+  patternName: any;
 
   constructor() { }
 
