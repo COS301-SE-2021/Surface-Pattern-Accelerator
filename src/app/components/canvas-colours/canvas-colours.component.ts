932dca845093caa449bf5c904434badf358c2b1a
olivia9469 <82371348+olivia9469@users.noreply.github.com>
2021-08-04 21:07:34 +0200

    implemented function that converts rgb to hex, for the colour extraction

--- src/app/components/canvas-colours/canvas-colours.component.ts
+++ src/app/components/canvas-colours/canvas-colours.component.ts
@@ -34,9 +34,17 @@ export class CanvasColoursComponent implements OnInit {
     const data = imageData.data;
     let i,n;
 
-
   }
 
+  rgbToHex(str: string){
+    let rgb = str.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+
+    function hex(x) {
+      return ("0" + parseInt(x).toString(16)).slice(-2);
+    }
+
+    return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+  }
 
 
 }
