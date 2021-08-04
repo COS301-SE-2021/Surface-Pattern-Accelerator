bd97fa1769351dfeb86959d533cd68e1b0a5320f
olivia9469 <82371348+olivia9469@users.noreply.github.com>
2021-08-04 21:01:37 +0200

    Added htmlToAdd variable for the angular innerHTML tag

--- src/app/components/canvas-colours/canvas-colours.component.ts
+++ src/app/components/canvas-colours/canvas-colours.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class CanvasColoursComponent implements OnInit {
 
   colourList = [];
+  htmlToAdd: any;
   constructor() {}
 
   ngOnInit() {
