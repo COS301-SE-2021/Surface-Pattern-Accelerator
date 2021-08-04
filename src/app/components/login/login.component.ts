43ef2bbf87d0244f5e67a740bbfeada145882f41
ODBabalola <u18041494@tuks.co.za>
2021-08-04 18:37:53 +0200

    Lazy loading attempt to hide tab bar

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../../services/login.service';
+import {NavigationEnd, Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +8,25 @@ import { LoginService } from '../../services/login.service';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  loggedIn: boolean = false;
+  loggedIn = false;
   email?: string;
   password?: string;
 
+  /*
+  This public variable is to prevent the tab bar
+  from appearing in the login screen
+   */
+  public currentUrl: string;
 
-
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService, private router: Router) {
+    // @ts-ignore
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        console.log('loading finished', event);
+        this.currentUrl = event.url.split('/').join('-');
+      }
+    });
+  }
 
   ngOnInit() {}
 
