6b13b28c448e38ca0edb2c5053e101149e0fd804
ODBabalola <u18041494@tuks.co.za>
2021-08-04 18:07:28 +0200

    Reverted failed fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,6 @@ import { CollorPalletComponent } from './components/collor-pallet/collor-pallet.
 import { LoginComponent } from './components/login/login.component';
 import { LoginResponseComponent} from './components/login-response/login-response.component';
 import { ColorComponent} from './components/color/color.component';
-import { TabsPageComponent } from './components/tabs-page/tabs-page.component';
 
 const routes: Routes = [
 
@@ -22,8 +21,7 @@ const routes: Routes = [
   { path: 'import', component: ImportComponent },
   { path: 'workarea', component: WorkareaComponent },
   { path: 'loginResponse', component: LoginResponseComponent },
-  { path: 'color', component: ColorComponent},
-  { path: 'tabs', component: TabsPageComponent}
+  { path: 'color', component: ColorComponent}
 ];
 
 @NgModule({
