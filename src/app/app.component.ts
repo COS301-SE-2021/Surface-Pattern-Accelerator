4efc4aa1f1da3e658fe68c5fc641022a2ebd6654
ODBabalola <u18041494@tuks.co.za>
2021-08-05 16:10:47 +0200

    New tab bar fix 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,10 @@ import {Component} from '@angular/core';
 })
 
 export class AppComponent {
-  constructor() {}
+  //failed attempt at hading a tab bar
+  //static tabBar: any = <HTMLElement>document.getElementById('tab-bar1');
+  static tabBar: any;
+constructor() {
+  AppComponent.tabBar = <HTMLElement>document.getElementById('tab-bar1');
+}
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../../services/login.service';
+import {AppComponent} from '../../app.component';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
+
 export class LoginComponent implements OnInit {
   loggedIn = false;
   email?: string;
   password?: string;
 
   constructor(private loginService: LoginService) {
-    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-    const tabBar: any = <HTMLElement>document.getElementById('tab-bar1');
-    tabBar.style.hidden = true;
+
+    /*
+    * Attempt at hiding tab bar in login page.
+     */
+    if (AppComponent.tabBar != null) {
+      AppComponent.tabBar.style.hidden = true;
+    }
+
   }
 
   ngOnInit() {}
