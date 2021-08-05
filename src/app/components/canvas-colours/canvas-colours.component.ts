807c2651dad7d4489446faa9e66fef851ef55d73
olivia9469 <82371348+olivia9469@users.noreply.github.com>
2021-08-05 14:09:28 +0200

    rgb to hex function update

--- src/app/components/canvas-colours/canvas-colours.component.ts
+++ src/app/components/canvas-colours/canvas-colours.component.ts
@@ -75,6 +75,13 @@ export class CanvasColoursComponent implements OnInit {
     }
   }
 
+  rgbToHex(str: string){
+    let rgb = str.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    function hex(x) {
+      return ("0" + parseInt(x).toString(16)).slice(-2);
+    }
+    return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+  }
 
 
 }
