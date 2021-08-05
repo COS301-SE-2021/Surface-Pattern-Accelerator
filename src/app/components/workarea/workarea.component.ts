7324ea0727d15f9f036aff76e32dad2d038036f5
EmilioCoelho-ui <u18194380@tuks.co.za>
2021-08-05 17:41:04 +0200

    imaging prototype

--- src/app/components/workarea/workarea.component.ts
+++ src/app/components/workarea/workarea.component.ts
@@ -74,6 +74,35 @@ export class WorkareaComponent implements OnInit {
     box.on('mouseout', function() {
       document.body.style.cursor = 'default';
     });
+
+    function drawImage(imageOb) {
+      // Image
+      const img = new Konva.Image({
+        image: imageOb,
+        x: this.stage.width() / 2 - 200 / 2,
+        y: this.stage.height() / 2 - 137 / 2,
+        width: 200,
+        height: 137,
+        draggable: true,
+      });
+// add cursor styling
+      // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+      img.on('mouseover', function() {
+        document.body.style.cursor = 'pointer';
+      });
+      // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+      img.on('mouseout', function() {
+        document.body.style.cursor = 'default';
+      });
+      this.layer.add(this.img);
+    }
+    const imageObj = new Image();
+    imageObj.onload = function() {
+      drawImage(this);
+    };
+    imageObj.src = '/assets/umbrella.jpg';
+
+
     this.layer.add(box);
     this.stage.add(this.layer);
     //this.addLineListeners();
@@ -120,48 +149,47 @@ export class WorkareaComponent implements OnInit {
       this.myFrame.style.height = this.height + 40 +  'px';
     }
   }
-generateMotif(){
-    //simple cloning of layer and adding it to stage
-  const rectX = this.stage.width() / 2 - 50;
-  const rectY = this.stage.height() / 2 - 50;
-  const box = new Konva.Rect({
-    x: rectX,
-    y: rectY,
-    width: 100,
-    height: 100,
-    fill: '#00D2FF',
-    stroke: 'black',
-    strokeWidth: 4,
-    draggable: true,
+  generateMotif(){
+      //simple cloning of layer and adding it to stage
+    const rectX = this.stage.width() / 2 - 50;
+    const rectY = this.stage.height() / 2 - 50;
+    const box = new Konva.Rect({
+      x: rectX,
+      y: rectY,
+      width: 100,
+      height: 100,
+      fill: '#00D2FF',
+      stroke: 'black',
+      strokeWidth: 4,
+      draggable: true,
+      // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+      dragBoundFunc(pos) {
+        let X = pos.x;
+        let Y = pos.y;
+        const minX = 0;
+        const minY = 0;
+        const maxX = 400;
+        const maxY = 400;
+        if(X < minX) {X = minX;}
+        if(X > maxX) {X = maxX;}
+        if(Y < minY) {Y = minY;}
+        if(Y > maxY) {Y = maxY;}
+        return {
+          x: X,
+          y: Y,
+        };
+      },
+    });
+    // add cursor styling
     // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
-    dragBoundFunc(pos) {
-      let X = pos.x;
-      let Y = pos.y;
-      const minX = 0;
-      const minY = 0;
-      const maxX = 400;
-      const maxY = 400;
-      if(X < minX) {X = minX;}
-      if(X > maxX) {X = maxX;}
-      if(Y < minY) {Y = minY;}
-      if(Y > maxY) {Y = maxY;}
-      return {
-        x: X,
-        y: Y,
-      };
-    },
-  });
-
-  // add cursor styling
-  // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
-  box.on('mouseover', function() {
-    document.body.style.cursor = 'pointer';
-  });
-  // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
-  box.on('mouseout', function() {
-    document.body.style.cursor = 'default';
-  });
-  this.layer.add(box);
-  this.stage.add(this.layer);
-}
+    box.on('mouseover', function() {
+      document.body.style.cursor = 'pointer';
+    });
+    // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+    box.on('mouseout', function() {
+      document.body.style.cursor = 'default';
+    });
+    this.layer.add(box);
+    this.stage.add(this.layer);
+  }
 }
