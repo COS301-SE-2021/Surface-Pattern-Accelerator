811f3256770ac325b74d6e29206ddfa129e702e8
Reghardt <reghardt1000@gmail.com>
2021-08-12 18:16:16 +0200

    Node - Small interface change

--- Interfaces/collectionContents.interface.ts
+++ Interfaces/collectionContents.interface.ts
@@ -1,4 +1,5 @@
 // this interface represents the physical structure of the collections file stored on the drive
+//must align with version on Angular
 
 export interface ICollectionsContent {
     collectionName: string;
