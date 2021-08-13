6fa213e1317dfab7deed3e486092c7e802547531
olivia9469 <82371348+olivia9469@users.noreply.github.com>
2021-08-13 19:52:22 +0200

    Complete unit tests for color extractor

--- src/app/components/canvas-colours/canvas-colours.component.spec.ts
+++ src/app/components/canvas-colours/canvas-colours.component.spec.ts
@@ -23,21 +23,21 @@ describe('CanvasColoursComponent', () => {
   });
 
   it('Test 1: Testing if correct method is called when the user clicks on the canvas',()=>{
-    spyOn(component, "canvasColour");
-    let canvasElement = fixture.debugElement.nativeElement.querySelector('#canvas');
+    spyOn(component, 'canvasColour');
+    const canvasElement = fixture.debugElement.nativeElement.querySelector('#canvas');
     canvasElement.click();
 
     fixture.whenStable().then(()=>{
       expect(component.canvasColour).toHaveBeenCalled();
-    })
+    });
   });
   it('Test 2: checking if canvas colour function call works correctly', ()=>{
-    let result = component.canvasColour();
+    const result = component.canvasColour();
     expect(result).toEqual(1);
   });
 
   it('Test 3: Testing if rgb to hex converter works correctly', ()=>{
-    let result = component.rgbToHex('rgb(219,24,163)');
+    const result = component.rgbToHex('rgb(219,24,163)');
     expect(result).toEqual('#DB18A3');
   });
 
