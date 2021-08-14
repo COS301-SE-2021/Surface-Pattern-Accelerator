fb810d948725b702e64d2f4d5fe95ac55d2c8b04
Reghardt <reghardt1000@gmail.com>
2021-08-14 18:17:52 +0200

    Popover created for basic collection operations

---
+++ src/app/popovers/collection-basic-operations/collection-basic-operations.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-collection-basic-operations',
+  templateUrl: './collection-basic-operations.component.html',
+  styleUrls: ['./collection-basic-operations.component.scss'],
+})
+export class CollectionBasicOperationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
