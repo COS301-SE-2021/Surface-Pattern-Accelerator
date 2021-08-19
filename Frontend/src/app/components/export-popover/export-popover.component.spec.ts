37667c09c4c5e25ffc829e428544cafe031ec64a
olivia9469 <82371348+olivia9469@users.noreply.github.com>
2021-08-19 17:25:39 +0200

    Removed error in export-popover.component.spec.ts

--- Frontend/src/app/components/export-popover/export-popover.component.spec.ts
+++ Frontend/src/app/components/export-popover/export-popover.component.spec.ts
@@ -18,17 +18,17 @@ describe('ExportPopoverComponent', () => {
     fixture.detectChanges();
   }));
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
-  //
-  // it('Test 1: Testing if correct method is called when ok button is clicked', () => {
-  //   spyOn(component, 'closePopover');
-  //   let btn1 = fixture.debugElement.nativeElement.querySelector('#ok-btn');
-  //   btn1.click();
-  //
-  //   fixture.whenStable().then(() => {
-  //     expect(component.closePopover).toHaveBeenCalled();
-  //   });
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('Test 1: Testing if correct method is called when ok button is clicked', () => {
+    spyOn(component, 'closePopover');
+    const btn1 = fixture.debugElement.nativeElement.querySelector('#ok-btn');
+    btn1.click();
+
+    fixture.whenStable().then(() => {
+      expect(component.closePopover).toHaveBeenCalled();
+    });
+  });
 });
