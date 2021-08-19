b326b39797fe452fec00ea9d8c9f5a205380e6b7
olivia9469 <82371348+olivia9469@users.noreply.github.com>
2021-08-19 16:16:41 +0200

    more testing

--- Frontend/src/app/app.component.spec.ts
+++ Frontend/src/app/app.component.spec.ts
@@ -2,11 +2,31 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 import {RouterTestingModule} from '@angular/router/testing';
 
+//components
 import { AppComponent } from './app.component';
 
+
+import { NewCollectionComponent } from './components/collection-creator/new-collection/new-collection.component';
+import { CollectionsComponent } from './components/collections/collections.component';
+
+//services
+import { CollectionsServiceService } from './services/collections-service.service';
+
 describe('AppComponent', () => {
-  let fixture: ComponentFixture<AppComponent>;
+  //Fixtures
+  let appFixture: ComponentFixture<AppComponent>;
+  let newCollectionFixture: ComponentFixture<NewCollectionComponent>;
+  let collectionServiceFixture: CollectionsServiceService;
+  let collectionFixture: ComponentFixture<CollectionsComponent>;
+
+  //Components
   let app: AppComponent;
+  let newCollection: NewCollectionComponent;
+  let collectionService: CollectionsServiceService;
+  let collectionComponent: CollectionsComponent;
+
+  //create a collection service
+  let collectionServiceM: Partial<CollectionsServiceService>;
 
   beforeEach(waitForAsync(() => {
 
@@ -14,26 +34,71 @@ describe('AppComponent', () => {
       imports: [RouterTestingModule],
       declarations: [AppComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [{
+        provide: CollectionsServiceService,
+        useValue: collectionServiceM
+      }]
     }).compileComponents();
   }));
 
-  beforeEach( () => {
-    fixture = TestBed.createComponent(AppComponent);
-    app = fixture.debugElement.componentInstance;
-  });
-
   it('should create the app', () => {
+    appFixture = TestBed.createComponent(AppComponent);
+    app = appFixture.debugElement.componentInstance;
+
     expect(app).toBeTruthy();
   });
 
   // TODO: integration tests in here
 
-  it('Integration test', () => {
+  it('Integration test: color picker component should be called from the newCollection component ', () => {
+
+    newCollectionFixture = TestBed.createComponent(NewCollectionComponent);
+
+    newCollection = newCollectionFixture.debugElement.componentInstance;
+
+    newCollectionFixture.whenStable().then(() => {
+
+      const element = newCollectionFixture.debugElement.nativeElement.querySelector('#newCol') as HTMLButtonElement;
+
+      element.click();
+
+      expect(newCollection.getColorCodes).toHaveBeenCalled();
+    });
   });
 
-  it('Integration test', () => {
+  // eslint-disable-next-line max-len
+  it('Integration test: testing to see if the new Collection component is interacting with the correct method in the collection service', () => {
+    newCollectionFixture = TestBed.createComponent(NewCollectionComponent);
+
+    newCollection = newCollectionFixture.debugElement.componentInstance;
+
+    newCollectionFixture.whenStable().then(() => {
+
+      collectionServiceFixture = TestBed.inject(CollectionsServiceService);
+
+      if(collectionServiceFixture) {
+
+        const element = newCollectionFixture.debugElement.nativeElement.querySelector('#newCol') as HTMLButtonElement;
+
+        element.click();
+
+        expect(collectionService.createNewCollection).toHaveBeenCalled();
+      }
+      });
+    expect().nothing();
   });
 
-  it('Integration test', () => {
+  it('Integration test: to see if the collections interacts with the new collection component',() => {
+
+    if(collectionFixture){
+
+      const el = collectionFixture.debugElement.nativeElement.querySelector('#redirect');
+
+      el.click();
+
+      expect(collectionComponent.navigateTo).toHaveBeenCalled();
+    }
+    expect().nothing();
   });
+
 });
