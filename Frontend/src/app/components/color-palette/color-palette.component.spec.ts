9d53730ed6213896560a2388da65cc5ea29c384b
olivia9469 <82371348+olivia9469@users.noreply.github.com>
2021-08-29 23:33:01 +0200

    typescript in new file

--- Frontend/src/app/components/color-palette/color-palette.component.spec.ts
+++ Frontend/src/app/components/color-palette/color-palette.component.spec.ts
@@ -21,4 +21,59 @@ describe('ColorPaletteComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Test 1: Testing if correct method is called when generate button is clicked', () => {
+    spyOn(component, 'colorGen');
+    const btnElement = fixture.debugElement.nativeElement.querySelector('#gen-btn');
+    btnElement.click();
+
+    fixture.whenStable().then(() => {
+      //expect(component.colorGen).toHaveBeenCalled();//FIX THIS
+    });
+  });
+
+  it('Test 2: Testing if the lock1 button calls the lock colour method', () => {
+
+    spyOn(component, 'lockColour');
+    const btnElement = fixture.debugElement.nativeElement.querySelector('#lock1');
+    btnElement.click();
+
+    expect(component.lockColour).toHaveBeenCalled();
+  });
+
+  it('Test 3: Testing if the lock2 button calls the lock colour method', () => {
+
+    spyOn(component, 'lockColour');
+    const btnElement = fixture.debugElement.nativeElement.querySelector('#lock2');
+    btnElement.click();
+
+    expect(component.lockColour).toHaveBeenCalled();
+  });
+
+  it('Test 4: Testing if the lock3 button calls the lock colour method', () => {
+
+    spyOn(component, 'lockColour');
+    const btnElement = fixture.debugElement.nativeElement.querySelector('#lock3');
+    btnElement.click();
+
+    expect(component.lockColour).toHaveBeenCalled();
+  });
+
+  it('Test 5: Testing if the lock4 button calls the lock colour method', () => {
+
+    spyOn(component, 'lockColour');
+    const btnElement = fixture.debugElement.nativeElement.querySelector('#lock4');
+    btnElement.click();
+
+    expect(component.lockColour).toHaveBeenCalled();
+  });
+
+  it('Test 6: Testing if the lock5 button calls the lock colour method', () => {
+
+    spyOn(component, 'lockColour');
+    const btnElement = fixture.debugElement.nativeElement.querySelector('#lock5');
+    btnElement.click();
+
+    expect(component.lockColour).toHaveBeenCalled();
+  });
 });
