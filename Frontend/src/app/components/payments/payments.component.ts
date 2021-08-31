b8854f4d85925cbbddf942c061ed93191b11658c
Bernadette Kebirungi <u17109435@tuks.co.za>
2021-08-31 16:20:18 +0200

    Payments component added

---
+++ Frontend/src/app/components/payments/payments.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payments',
+  templateUrl: './payments.component.html',
+  styleUrls: ['./payments.component.scss'],
+})
+export class PaymentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
