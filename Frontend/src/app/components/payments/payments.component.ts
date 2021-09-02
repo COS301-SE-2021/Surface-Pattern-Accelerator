83df7d2f4eb2865f0f7743ed3f40f4d6f5d0602a
Bernadette Kebirungi <u17109435@tuks.co.za>
2021-09-02 09:08:16 +0200

    generate payment signature

--- Frontend/src/app/components/payments/payments.component.ts
+++ Frontend/src/app/components/payments/payments.component.ts
@@ -9,6 +9,28 @@ export class PaymentsComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    const crypto = require("crypto");
+
+    const generateSignature = (data, passPhrase = null) => {
+      // Create parameter string
+      let pfOutput = "";
+      for (let key in data) {
+        if(data.hasOwnProperty(key)){
+          if (data[key] !== "") {
+            pfOutput +=`${key}=${encodeURIComponent(data[key].trim()).replace(/%20/g, "+")}&`
+          }
+        }
+      }
+
+      // Remove last ampersand
+      let getString = pfOutput.slice(0, -1);
+      if (passPhrase !== null) {
+        getString +=`&passphrase=${encodeURIComponent(passPhrase.trim()).replace(/%20/g, "+")}`;
+      }
+
+      return crypto.createHash("md5").update(getString).digest("hex");
+    };
+  }
 
 }
