932e73e6b6ea91f4d03c0ba3f58f3576b9de9650
ODBabalola <u18041494@tuks.co.za>
2021-09-04 16:29:42 +0200

    Further enhancements

--- Frontend/src/app/components/models/models.component.ts
+++ Frontend/src/app/components/models/models.component.ts
@@ -29,36 +29,22 @@ export class ModelsComponent implements OnInit {
     renderer.setClearColor("#e5e5e5");	// Background color
     renderer.setSize(window.innerWidth,window.innerHeight);
 
-    const controls = new OrbitControls( camera, renderer.domElement );
-    controls.enableDamping = true;
-    //controls.enablePan = false;
-    //camera.position.set( 0, 20, 1 );
-    controls.update();
-
-    // Create canvas element with our renderer settings
     document.body.appendChild(renderer.domElement);
+
     // Make the created canvas responsive
     window.addEventListener('resize', () => {
       renderer.setSize(window.innerWidth,window.innerHeight);
-      // We need to re-adjust the camera 'aspect ratio'
       camera.aspect = window.innerWidth/window.innerHeight;
-      // Needs to be called after an adjustment is made
       camera.updateProjectionMatrix();
     })
 
-    // Defining a native 3D object
-    // Shape and Form of object
+
     let geometry = new THREE.SphereGeometry(1, 10, 10);
-    // Material of Object
-    // Color of object is also defined here
-    // MeshLambertMaterial is a material for non-shiny surfaces, without spectacular highlights
     let material = new THREE.MeshLambertMaterial({color: 0xFFCC00})
-    //We need to combine the geometry and material into was called a mesh
     let mesh = new THREE.Mesh(geometry, material);
-    // To be able to move around an object in space
-    mesh.position.x = 5;
-    //Add the mesh to the scene
+    mesh.position.x = 2;
     //scene.add(mesh);
+
 
     // Defining the loader object to load 3D Models
     let loader = new GLTFLoader();
@@ -72,24 +58,15 @@ export class ModelsComponent implements OnInit {
     scene.add(gltf.scene);
     });
 
-
-    // Define the light
-    // Set the color, intensity and distance
     let light = new THREE.PointLight(0xFFFFFF,1,500);
     light.position.set(10,0,25);
     scene.add(light);
-    // Keeps the aspect ratio constant
-    // Br causing the renderer to redraw the screen everytime it is refreshed
+
     let render = function () {
       requestAnimationFrame(render);
-      // Rotates the object on the x-axis everytime the renderer function is called
-      // which is essentially 60 frames per second
-      mesh.rotation.x += 0.01;
+      //mesh.rotation.x += 0.01;
       obj.rotation.x += 0.01;
-      //obj.rotation.y += 0.01;
-      // required if controls.enableDamping or controls.autoRotate are set to true
-      controls.update();
-      // We have to call the renderer method on the renderer
+      obj.rotation.y += 0.01;
       renderer.render(scene, camera);
     }
 
