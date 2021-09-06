da164b790712640a072a34c69efec568bbe2bbfc
Bernadette Kebirungi <u17109435@tuks.co.za>
2021-09-06 11:59:18 +0200

    stripe api call added

--- Backend/test.ts
+++ Backend/test.ts
@@ -17,6 +17,11 @@ import {rejects} from "assert";
 import multer from "multer" ;
 import {ICollectionsInterface} from "./Interfaces/collections.interface";
 
+// stripe payment
+/// declare function require(name:string);
+import stripe = require("stripe")("sk_test_51JWIR0GnSZPbZIbcuxh2UOQBVHpVRQeFf4KagJ18wjYP9Rz0L2qs72idNwWjLNBi02563n0E2YqQysHxa7xzuUoa00yE0zX9Ml");
+
+/// const YOUR_DOMAIN = "http://localhost:8100";
 // tslint:disable-next-line:interface-name
 declare module "express-session" { interface Session { accessToken: ITokenInterface; } }
 
@@ -320,6 +325,29 @@ app.post("/api/uploadMotif", upload.array("files"), (req, res) => {
         });
 
 });
+
+/////////////////////////////////////////////////////////////// PAYMENT
+app.post("/api/create-checkout-session", async (req, res) => {
+    const YOUR_DOMAIN = "http://localhost:8100";
+    const session = await stripe.checkout.sessions.create({
+                line_items: [
+                    {
+                        // TODO: replace this with the `price` of the product you want to sell
+                        price: "price_1JWJO7GnSZPbZIbccDcaukYW",
+                        quantity: 1,
+                    },
+                ],
+                payment_method_types: [
+                    "card",
+                    "alipay"
+                ],
+                mode: "payment",
+                success_url: `${YOUR_DOMAIN}/success.html`,
+                cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+            });
+
+    res.redirect(303, session.url);
+        });
 
 // start the Express server
 app.listen(port, () => {
