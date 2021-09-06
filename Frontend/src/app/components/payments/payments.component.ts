d10a61e1406a28402f053f1e254b6dca5bfb0d55
Bernadette Kebirungi <u17109435@tuks.co.za>
2021-09-06 18:01:12 +0200

    stripe payment function edited

--- Frontend/src/app/components/payments/payments.component.ts
+++ Frontend/src/app/components/payments/payments.component.ts
@@ -7,48 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentsComponent implements OnInit {
 
+  paymentHandler:any = null;
+
   constructor() { }
 
   ngOnInit() {
-//     const crypto = require("crypto");
-//
-//     const myData = [];
-// // Merchant details
-//     myData["merchant_id"] = "10000100";
-//     myData["merchant_key"] = "46f0cd694581a";
-//     myData["return_url"] = "http://www.yourdomain.co.za/return_url";
-//     myData["cancel_url"] = "http://www.yourdomain.co.za/cancel_url";
-//     myData["notify_url"] = "http://www.yourdomain.co.za/notify_url";
-// // Buyer details
-//     myData["name_first"] = "First Name";
-//     myData["name_last"] = "Last Name";
-//     myData["email_address"] = "test@test.com";
-// // Transaction details
-//     myData["m_payment_id"] = "1234";
-//     myData["amount"] = "10.00";
-//     myData["item_name"] = "Order#123";
-//
-//     const generateSignature = (data, passPhrase = null) => {
-//       // Create parameter string
-//       let pfOutput = "";
-//       for (let key in data) {
-//         if(data.hasOwnProperty(key)){
-//           if (data[key] !== "") {
-//             pfOutput +=`${key}=${encodeURIComponent(data[key].trim()).replace(/%20/g, "+")}&`
-//           }
-//         }
-//       }
-//
-//       // Remove last ampersand
-//       let getString = pfOutput.slice(0, -1);
-//       if (passPhrase !== null) {
-//         getString +=`&passphrase=${encodeURIComponent(passPhrase.trim()).replace(/%20/g, "+")}`;
-//       }
-//
-//       return crypto.createHash("md5").update(getString).digest("hex");
-//     };
-
-
+    this.invokeStripe();
   //   //stripe payment
   //   const stripe = require('stripe')('sk_test_51JWIR0GnSZPbZIbcuxh2UOQBVHpVRQeFf4KagJ18wjYP9Rz0L2qs72idNwWjLNBi02563n0E2YqQysHxa7xzuUoa00yE0zX9Ml');
   //   const express = require('express');
@@ -79,20 +43,44 @@ export class PaymentsComponent implements OnInit {
   //   });
   }
 
-//   // Generate signature
-//   myData["signature"] = generateSignature(this.myData);
-//
-//   let htmlForm = `<form action="https://${pfHost}/eng/process" method="post">`;
-//   for (let key in myData) {
-//   if(myData.hasOwnProperty(key)){
-//   value = myData[key];
-//   if (value !== "") {
-//   htmlForm +=`<input name="${key}" type="hidden" value="${value.trim()}" />`;
-//   }
-//     }
-// }
-//
-// htmlForm += '<input type="submit" value="Pay Now" /></form>';
+  makePayment(amount) {
+    const paymentHandler = (<any>window).StripeCheckout.configure({
+      key: 'pk_test_51JWIZsHgVOPspuvRuKmrbhqQPeDeAwXgEhDYztocJWUJEYGI5b5Ciy2w5S7sXTgJ5qb1bLV0nOiTzYee6tscNPyS00hb8s0dAg',
+      locale: 'auto',
+      token: function (stripeToken: any) {
+        console.log(stripeToken)
+        alert('Stripe token generated!');
+      }
+    });
+
+    paymentHandler.open({
+      name: 'Surface Pattern Accelerator',
+      description: '3 widgets',
+      amount: amount * 100
+    });
+  }
+
+  invokeStripe() {
+    if(!window.document.getElementById('stripe-script')) {
+      const script = window.document.createElement("script");
+      script.id = "stripe-script";
+      script.type = "text/javascript";
+      script.src = "https://checkout.stripe.com/checkout.js";
+      script.onload = () => {
+        this.paymentHandler = (<any>window).StripeCheckout.configure({
+          key: 'pk_test_51JWIZsHgVOPspuvRuKmrbhqQPeDeAwXgEhDYztocJWUJEYGI5b5Ciy2w5S7sXTgJ5qb1bLV0nOiTzYee6tscNPyS00hb8s0dAg',
+          locale: 'auto',
+          token: function (stripeToken: any) {
+            console.log(stripeToken)
+            alert('Payment has been successfull!');
+          }
+        });
+      }
+
+      window.document.body.appendChild(script);
+    }
+  }
+
 
 
 }
