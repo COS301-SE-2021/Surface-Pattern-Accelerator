15426af57a998ed50baeb1c996bf737710de26be
olivia9469 <82371348+olivia9469@users.noreply.github.com>
2021-09-12 21:52:51 +0200

    adding new files for style transfer

---
+++ Frontend/src/app/components/style-transfer/style-transfer.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-style-transfer',
+  templateUrl: './style-transfer.component.html',
+  styleUrls: ['./style-transfer.component.scss'],
+})
+export class StyleTransferComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
