ad0a853018696fd715191564f88294d0c09ef45b
JohanDC-CS <u18200754@tuks.co.za>
2021-09-19 15:00:52 +0200

    payment guard

--- Frontend/src/app/services/payment-guard.guard.ts
+++ Frontend/src/app/services/payment-guard.guard.ts
@@ -1,14 +1,33 @@
 import { Injectable } from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
 import { Observable } from 'rxjs';
+import {GLoginService} from "./g-login.service";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentGuardGuard implements CanActivate {
   hasPayed: boolean = false;
-  constructor(public router: Router) {}
+  private serverURL = 'http://localhost:3000';
+  constructor(public router: Router, private gLoginService: GLoginService, private http: HttpClient) {}
   canActivate(){
+    if(this.gLoginService.getUserDetails() && this.gLoginService.getUserDetails().isSignedIn())
+    {
+
+    this.http.post(this.serverURL + '/api/payment' ,
+      { email: this.gLoginService.getUserDetails().getBasicProfile().getEmail()},
+      {withCredentials: true})
+      .subscribe(data=>{
+        console.log(data);
+      })
+
+
+    }
+
+
+
+
 
     // if(this.hasPayed == false)
     // {
