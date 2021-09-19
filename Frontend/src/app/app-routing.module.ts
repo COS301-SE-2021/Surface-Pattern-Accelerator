b773b18bdb7cceca747cfa2b13e24c3089dee5e1
JohanDC-CS <u18200754@tuks.co.za>
2021-09-19 15:29:53 +0200

    app routing guard

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -13,10 +13,12 @@ import { ColorComponent} from './components/color/color.component';
 import { NewCollectionComponent } from './components/collection-creator/new-collection/new-collection.component';
 import { MainComponent } from './components/launchpage/main/main.component';
 import {StyleTransferComponent} from './components/style-transfer/style-transfer.component';
+
 //import { ExportPopoverComponent } from './components/export-popover/export-popover.component';
 import {GLoginComponent} from './components/g-login/g-login.component';
 import {MotifEditorComponent} from './components/motif-editor/motif-editor.component';
 import {PaymentComponent} from "./components/payment/payment.component";
+import{ PaymentGuardGuard} from "./services/payment-guard.guard";
 
 const routes: Routes = [
 
@@ -25,15 +27,15 @@ const routes: Routes = [
   // { path: '',    redirectTo: 'login',    pathMatch: 'full'  },
   { path: 'collections', component: CollectionsComponent}, //if a url matches this path then the appropriate component wil be displayed
   { path: 'collectionCreator', component: CollectionCreatorComponent},
-  { path: 'color-palette', component: ColorPaletteComponent},
+  { path: 'color-palette', component: ColorPaletteComponent,canActivate: [PaymentGuardGuard]},
   { path: 'pattern', component: PatternComponent},
   { path: 'workarea', component: WorkareaComponent },
   { path: 'loginResponse', component: LoginResponseComponent },
-  { path: 'canvasColours', component: CanvasColoursComponent },
+  { path: 'canvasColours', component: CanvasColoursComponent,canActivate: [PaymentGuardGuard] },
   { path: 'preview', component: PreviewComponent },
-  { path: 'color', component: ColorComponent},
+  { path: 'color', component: ColorComponent,canActivate: [PaymentGuardGuard]},
   { path: 'newCollection', component: NewCollectionComponent },
-  { path: 'style-transfer', component: StyleTransferComponent },
+  { path: 'style-transfer', component: StyleTransferComponent,canActivate: [PaymentGuardGuard]},
   { path: 'newCollection', component: NewCollectionComponent },
   { path: 'glogin', component: GLoginComponent},
   { path: 'motifEditor', component: MotifEditorComponent}, //TODO: potentially add route same as pattern component
