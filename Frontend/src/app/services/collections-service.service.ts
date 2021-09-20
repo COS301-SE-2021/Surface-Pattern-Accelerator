9b9ac611458585d77a58b1eddfe3bb0844778d93
Reghardt <reghardt1000@gmail.com>
2021-09-20 11:08:00 +0200

    server link example

--- Frontend/src/app/services/collections-service.service.ts
+++ Frontend/src/app/services/collections-service.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {ICollectionsContent} from '../Interfaces/collectionContents.interface';
+import {ServerLinkService} from "./server-link.service";
 
 
 @Injectable({
@@ -12,22 +13,22 @@ export class CollectionsServiceService {
 
   public currentCollectionID: string;
 
-  private serverURL = 'http://localhost:3000';
+
   //private serverURL = 'http://ec2-3-128-186-246.us-east-2.compute.amazonaws.com:3000';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private serverLink: ServerLinkService) { }
 
 
   getCollections(): Observable<ICollectionsContent[]>
   {
     console.log('get collections fired!');
-    const getCollectionsURL = this.serverURL + '/api/getCollections';
+    const getCollectionsURL = this.serverLink.getServerLink() + '/api/getCollections';
     return this.http.get<ICollectionsContent[]>(getCollectionsURL, {withCredentials: true}); //GET request
   }
 
   /** POST: add a new collection to the server */
    createNewCollection(collectionName: string) {
-    const newCollectionURL = this.serverURL + '/api/newCollection';
+    const newCollectionURL = this.serverLink.getServerLink() + '/api/newCollection';
     console.log(newCollectionURL);
     return this.http.post<any>(newCollectionURL,
       {collectionName},
