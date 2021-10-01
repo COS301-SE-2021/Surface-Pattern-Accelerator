3bc45dbeed9a49e168234cd2001080100eb41965
JohanDC-CS <u18200754@tuks.co.za>
2021-10-01 08:33:56 +0200

    Changed collection component to use ThemeService

--- Frontend/src/app/components/collections/collections.component.ts
+++ Frontend/src/app/components/collections/collections.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { CollectionsServiceService } from '../../services/collections-service.service';
 import {Router} from '@angular/router';
 import { LoadingController } from '@ionic/angular';
@@ -9,20 +9,30 @@ import {ICollectionsContent} from '../../Interfaces/collectionContents.interface
 import { MenuController } from '@ionic/angular';
 
 import {MotifUploadComponent} from "../../popovers/motif-upload/motif-upload.component";
+import {ThemeServiceService} from "../../services/theme-service.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-collections',
   templateUrl: './collections.component.html',
   styleUrls: ['./collections.component.scss'],
 })
-export class CollectionsComponent implements OnInit {
+export class CollectionsComponent implements OnInit, OnDestroy {
 
   collections?: ICollectionsContent[] = []; //the collections that get displayed, marked as optional
   activeComponent: string;
-
+  private subscription: Subscription;
   menuController: MenuController;
+
+  background_dark: string;
+  background_light: string;
+  color_dark: string;
+  color_light: string;
+  secondaryBackground_dark: string = "#404040";
+  secondaryBackground_light: string = "white";
+  listener;
   // eslint-disable-next-line max-len
-  constructor(private collectionsService: CollectionsServiceService, private router: Router, public loadingController: LoadingController, private popoverController: PopoverController)
+  constructor(private collectionsService: CollectionsServiceService, private router: Router, public loadingController: LoadingController, private popoverController: PopoverController, private themeService: ThemeServiceService)
   {
 
   }
@@ -31,14 +41,26 @@ export class CollectionsComponent implements OnInit {
 
   ngOnInit(): void
   {
+    this.listener = this.themeService.darkModeChange;
+
     this.menuController = new MenuController();
     this.menuController.enable(true, 'main-content');
     this.getCollections();
     this.activeComponent = 'Collections';
     this.setActive("Collections");
-   //  this.menuController = new MenuController();
-   //  this.menuController.enable(true, 'main-content');
-  //  this.getCollections();
+
+    this.changeTheme();
+
+
+    // this.themeService.isDarkMode.
+    this.subscription = this.themeService.darkModeChange.subscribe(value => {
+      this.changeTheme();
+    });
+
+
+  }
+  ngOnDestroy() {
+   this.subscription.unsubscribe();
   }
 
   CollectionOperations(ev: any)
@@ -109,5 +131,52 @@ export class CollectionsComponent implements OnInit {
     console.log(collectionID)
     this.collectionsService.currentCollectionID = collectionID;
     this.router.navigate(['pattern']).then()
+  }
+
+  changeTheme(){
+    let card =  document.getElementsByClassName('card');
+    let heading  = document.getElementById('heading');
+    let left =  document.getElementsByClassName('color');
+    let right =  document.getElementsByClassName('right');
+    let patternNum = document.getElementsByClassName('patterblock');
+    let button = document.getElementById('addBtn');
+
+
+    if(this.themeService.isDarkMode == false)
+    {
+      console.log('light');
+      for(let i = 0 ; i < card.length ; i++){
+        (<HTMLElement> card[i]).style.backgroundColor = "#F7F7F9";
+      }
+      let r = document.querySelector(':root');
+      (<HTMLElement>r).style.setProperty('--mycolor', this.secondaryBackground_light);
+      (<HTMLElement>r).style.setProperty('--shadowcolorDark', 'rgba(0,0,0,0.25)');
+      (<HTMLElement>r).style.setProperty('--shadowcolorLight', 'rgba(0,0,0,0.23)');
+      // for(let i = 0 ; i < left.length ; i++){
+      //   (<HTMLElement> left[i]).style.backgroundColor = this.secondaryBackground_light;
+      //
+      // }
+
+      (<HTMLElement> heading).style.color = "black";
+      (<HTMLElement> button).style.color = "blue";
+
+
+
+    }
+    else{
+      for(let i = 0 ; i < card.length ; i++){
+        (<HTMLElement> card[i]).style.backgroundColor = "#181818";
+        console.log('dark');
+      }
+      // for(let i = 0 ; i < left.length ; i++){
+      //   (<HTMLElement> left[i]).style.backgroundColor = this.secondaryBackground_dark;
+      // }
+      let r = document.querySelector(':root');
+      (<HTMLElement>r).style.setProperty('--mycolor', this.secondaryBackground_dark);
+      (<HTMLElement>r).style.setProperty('--shadowcolorDark', 'rgba(255,255,255,0.25)');
+      (<HTMLElement>r).style.setProperty('--shadowcolorLight', 'rgba(255,255,255,0.23)');
+      (<HTMLElement> heading).style.color = "white";
+      (<HTMLElement> button).style.color = "white";
+    }
   }
 }
