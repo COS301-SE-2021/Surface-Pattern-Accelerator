a53319ac5f8aa525a496662e8610e3bda4706656
JohanDC-CS <u18200754@tuks.co.za>
2021-10-01 08:32:07 +0200

    App component changes for dark mode toggle

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {ThemeServiceService} from "./services/theme-service.service";
 
 @Component({
   selector: 'app-root',
@@ -8,21 +9,27 @@ import {Router} from '@angular/router';
 })
 
 export class AppComponent implements OnInit {
-constructor(private router: Router) {
+  constructor(private router: Router, private themeService: ThemeServiceService) {
 
-}
+  }
 
   ngOnInit(): void {
-  console.log('this.router.url', this.router.url);
-    document.body.setAttribute('color-theme', 'light');//set it automatically for values later
+    console.log('this.router.url', this.router.url);
+    this.themeService.makeDark(false);
   }
 
   toggleTheme(event){
     if(event.detail.checked){
       document.body.setAttribute('color-theme', 'dark');
+      this.themeService.makeDark(true);
+      this.themeService.darkModeChange.next(true);
     }
     else{
       document.body.setAttribute('color-theme', 'light');
+      this.themeService.makeDark(false);
+      this.themeService.darkModeChange.next(false);
     }
+
+
   }
 }
