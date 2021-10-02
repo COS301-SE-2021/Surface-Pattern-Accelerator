0965d75d8977e421114036b54462de5ca2537bb7
ODBabalola <u18041494@tuks.co.za>
2021-10-03 01:05:21 +0200

    Export model object

--- backend-nest/MODEL/JS/main.js
+++ backend-nest/MODEL/JS/main.js
@@ -235,3 +235,22 @@ function startAnimation() {
   }
   render();
 }
+
+function exportObject() {
+  const exporter = new THREE.OBJExporter();
+  const result = exporter.parse( scene );
+  download("model.obj",result);
+}
+
+function download(filename, text) {
+  let element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
