d64ca6cb071265ab856900f6f628a23b9e345520
ODBabalola <u18041494@tuks.co.za>
2021-10-03 02:48:11 +0200

    3D Viewer UI updates

--- backend-nest/MODEL/JS/main.js
+++ backend-nest/MODEL/JS/main.js
@@ -254,3 +254,15 @@ function download(filename, text) {
 
   document.body.removeChild(element);
 }
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+
+//canvasPicture-4cfacanvasPicture.png
